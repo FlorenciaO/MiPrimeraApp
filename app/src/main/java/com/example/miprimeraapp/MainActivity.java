5fe955a5b08af6a85d68d7de289a00314673956c
Florencia Olivera <o.florencia123@gmail.com>
2024-12-03 18:14:53 -0300

    Primer layout XML + Uso de TextView y Button

--- app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.miprimeraapp;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "MainActivity";
 
-    private Button btnIniciarSesion;
-
+    private TextView numeroTV;
+    private Button incrementarBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +22,15 @@ public class MainActivity extends AppCompatActivity {
 
         /* Vinculación de elementos */
 
-        btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
+        incrementarBtn = findViewById(R.id.incrementarBtn);
         // TODO(1.Completar vinculación de elementos.)
 
         /* Configurar vista + acciones */
 
-        // TODO(2.Configurar el texto del botón "Crear Usuario")
-
-        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+        incrementarBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "El botón Iniciar Sesión fue clickeado", Toast.LENGTH_LONG).show();
-
-                // TODO(3.Pequeña validación de que los campos ingresados no estén vacíos. De lo contrario, mostrar un mensaje de error.)
+                // TODO(2. Incrementar el numero y mostrar el nuevo valor en la pantalla)
             }
         }
         );
